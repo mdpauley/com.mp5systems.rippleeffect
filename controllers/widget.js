a93dcadaa0faeba6beebce5b97e3a9cb04be0b1d
Michael Pauley <mdpauley@gmail.com>
2014-12-24 12:34:09 -0500

    Set ripple properties using .applyProperties

--- controllers/widget.js
+++ controllers/widget.js
@@ -3,38 +3,37 @@ $.create = function (e){
 	// This way we can make the circle big enough to fit the view.
 	var rippleWidth = e.source.toImage().width*2;
 	var rippleHeight = e.source.toImage().width*2;
-	
+
 	// Our circle that will be scaled up using 2dMartix.
 	// We'll position the view at the center of the click position, by using (clickPositin - (clickedViewWidth / 2)).
-	$.ripple.setTop(e.y-(rippleWidth/2));
-	$.ripple.setLeft(e.x-(rippleWidth/2));
-	$.ripple.setWidth(rippleWidth);
-	$.ripple.setHeight(rippleHeight);
-	$.ripple.setBorderRadius(rippleWidth/2);
-	$.ripple.setBackgroundColor("rgba(255,255,255,0.40)");
-	$.ripple.setTransform(Titanium.UI.create2DMatrix().scale(0));
-	$.ripple.setZindex(999);
-	$.ripple.setOpacity(0);
-			
+	$.ripple.applyProperties({
+		"top": e.y-(rippleWidth/2),
+		"left": e.x-(rippleWidth/2),
+		"width": rippleWidth,
+		"height": rippleHeight,
+		"borderRadius": rippleWidth/2,
+		"transform": Titanium.UI.create2DMatrix().scale(0)
+	});
+
 	// Add the ripple view inside the clicked view
 	e.source.add($.ripple);
-	
+
 	// Our animation that will set the opasity from 0 to 1, and scale our view to 50% of max size.
 	$.ripple.animate({
 		opacity:1,
 		transform:Titanium.UI.create2DMatrix().scale(0.5),
 		duration:200,
 		curve:Titanium.UI.ANIMATION_CURVE_EASE_IN_OUT
+	},function() {
+		// When the first animation is finish we'll scale up to 100% while we fade the opacity to 0.
+		$.ripple.animate({
+			opacity:0,
+			transform:Titanium.UI.create2DMatrix().scale(1),
+			duration:200,
+			curve:Titanium.UI.ANIMATION_CURVE_EASE_OUT_IN
 		},function() {
-			// When the first animation is finish we'll scale up to 100% while we fade the opacity to 0.
-			$.ripple.animate({
-				opacity:0,
-				transform:Titanium.UI.create2DMatrix().scale(1),
-				duration:200,
-				curve:Titanium.UI.ANIMATION_CURVE_EASE_OUT_IN
-				},function() {
-					// At the end we'll remove our ripple view from the clicked view.
-					e.source.remove($.ripple);
-				});
+			// At the end we'll remove our ripple view from the clicked view.
+			e.source.remove($.ripple);
 		});
+	});
 };
